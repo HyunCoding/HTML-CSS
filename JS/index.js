b333b48268574d31fcb3f609cca8559609ede0cd
HyunYong <aa01080809571@gmail.com>
2021-10-12 15:25:41 +0900

    10/12 HTML

---
+++ JS/index.js
@@ -0,0 +1,7 @@
+function toggleText() {
+    var text = document.getElementById("demo");
+    if (text.style.display == "none") {
+        text.style.display = "block";}
+    else {
+        text.style.display = "none";}
+}
